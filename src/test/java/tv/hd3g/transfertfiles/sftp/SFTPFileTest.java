0c4b7748ad8df8ccbbca0904ecd4b6226a0bc5e7
hdsdi3g <admin@hd3g.tv>
2021-10-12 15:29:38 +0200

    Update sshd-sftp version from 2.5.1 to 2.7.0 (test purpose), and force slf4j-api version to 1.7.32 (test purpose again) #46

--- src/test/java/tv/hd3g/transfertfiles/sftp/SFTPFileTest.java
+++ src/test/java/tv/hd3g/transfertfiles/sftp/SFTPFileTest.java
@@ -29,7 +29,7 @@ import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
 import org.apache.sshd.server.shell.UnknownCommandFactory;
-import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
